7cf21ea42cfcaf2641eafa2675a9c1504bd09f5a
Nguyen Bence <bence325@gmail.com>
2021-09-09 00:10:28 +0200

    feat: add empty templates

---
+++ Templates/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Photos</title>
+    </head>
+    <body>
+        <?php
+        require_once "header.php";
+        require_once "content.php";
+        require_once "footer.php";
+        ?>
+    </body>
+</html>
