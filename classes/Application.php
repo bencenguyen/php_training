3405a615b2ec63cf8c8becc46f01d8351e2f6710
Nguyen Bence <bence325@gmail.com>
2021-09-12 03:41:45 +0200

    feat: add new Application class and delete app.php

---
+++ classes/Application.php
@@ -0,0 +1,34 @@
+<?php
+
+class Application {
+
+    public function start() {
+        ob_start();
+
+        $uri = $_SERVER["REQUEST_URI"];
+        $cleaned = explode("?", $uri)[0];
+        route('/php_training/', 'homeController');
+        route('/php_training/about', 'aboutController');
+        route('/php_training/image/(?<id>[\d]+)', 'singleImageController');
+        route('/php_training/image/(?<id>[\d]+)/edit', 'singleImageEditController', "POST");
+        route('/php_training/image/(?<id>[\d]+)/delete', 'singleImageDeleteController', "POST");
+
+        route('/php_training/login', 'loginFormController');
+        route('/php_training/logout', 'logoutSubmitController');
+        route('/php_training/login', 'loginSubmitController', "POST");
+
+        list($view, $data) = dispatch($cleaned, 'notFoundController');
+
+        if (preg_match("%^redirect\:%", $view)) {
+            $redirectTarget = substr($view, 9);
+            header("Location:".$redirectTarget);
+            die;
+        }
+
+        extract($data);
+        $user = createUser();
+        ob_clean();
+
+        require_once "templates/layout.php";
+    }
+}
