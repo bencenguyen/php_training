e8c7efad1b8c172ed3105ab83b93079f544d22ed
Nguyen Bence <bence325@gmail.com>
2021-09-12 21:41:49 +0200

    feat: add ResponseEmitter class

--- classes/ResponseEmitter.php
+++ classes/ResponseEmitter.php
@@ -5,5 +5,29 @@ class ResponseEmitter
 
     public function emit(Response $response)
     {
+        $this->emitStatusLine($response->getStatusCode(), $response->getReasonPhrase());
+        $this->emitHeaders($response->getHeaders());
+        $this->emitBody($response->getBody());
+    }
+
+    private function emitStatusLine(int $statusCode, string $reasonPhrase)
+    {
+        header(sprintf(
+            "HTTP/1.1 %d%s",
+            $statusCode,
+            $reasonPhrase
+        ), true, $statusCode);
+    }
+
+    private function emitHeaders(array $headers)
+    {
+        foreach ($headers as $key => $value) {
+            header(sprintf("%s: %s", $key, $value));
+        }
+    }
+
+    private function emitBody(string $body)
+    {
+        echo $body;
     }
 }
