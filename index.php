e09cd4acf46ae6c3d99e86dbd7fd2c3a3cd1f0b6
Nguyen Bence <bence325@gmail.com>
2021-09-13 22:10:28 +0200

    feat: use autoloader instead of require_once

--- index.php
+++ index.php
@@ -6,16 +6,10 @@ session_start();
 error_reporting(E_ALL);
 ini_set("display_errors", "1");
 
-require_once "classes/Photo.php";
 require_once "core/config.php";
 require_once "core/functions.php";
 require_once "core/controllers.php";
-require_once "classes/Dispatcher.php";
-require_once "classes/Application.php";
-require_once "classes/Response.php";
-require_once "classes/ResponseEmitter.php";
-require_once "classes/ResponseFactory.php";
-require_once "classes/ViewRenderer.php";
-require_once "classes/ModelAndView.php";
 
-(new Application())->start();
+require_once "autoload.php";
+
+(new \Application())->start();
